1473c8afeefa0f8ed57ee4a7a7b36b7adc28070a
Safal Piya <mrsafalpiya@gmail.com>
2026-04-09 01:47:06 +0545

    Integrate ASP.NET Core Identity with PostgreSQL

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
+
+namespace VehiclePartsIMS_Backend.Data
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser<int>, IdentityRole<int>, int>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityUser<int>>().ToTable("Users");
+            builder.Entity<IdentityRole<int>>().ToTable("Roles");
+            builder.Entity<IdentityUserRole<int>>().ToTable("UserRoles");
+
+            SeedRoles(builder);
+            SeedAdmin(builder);
+        }
+
+        public void SeedRoles(ModelBuilder builder)
+        {
+            List<IdentityRole<int>> roles = [
+                new IdentityRole<int> {
+                    Id = 1,
+                    Name = "Admin",
+                    NormalizedName = "ADMIN",
+                    ConcurrencyStamp = "133257ba-cf42-4cd4-a326-5b513272e9de"
+                },
+                new IdentityRole<int> {
+                    Id = 2,
+                    Name = "Staff",
+                    NormalizedName = "STAFF",
+                    ConcurrencyStamp = "7485b776-cf12-45ca-9a6a-40aa8b58c98b",
+                },
+                new IdentityRole<int> {
+                    Id = 3,
+                    Name = "Customer",
+                    NormalizedName = "CUSTOMER",
+                    ConcurrencyStamp = "797dd241-a38a-4a0e-867c-75b944a37300",
+                },
+            ];
+
+            builder.Entity<IdentityRole<int>>().HasData(roles);
+        }
+
+        public void SeedAdmin(ModelBuilder builder)
+        {
+            var admin = new IdentityUser<int>
+            {
+                Id = 1,
+                UserName = "admin@admin.com",
+                NormalizedUserName = "ADMIN@ADMIN.COM",
+                Email = "admin@admin.com",
+                NormalizedEmail = "ADMIN@ADMIN.COM",
+                EmailConfirmed = true,
+                PasswordHash = "AQAAAAIAAYagAAAAEHJuboYiX8TZciAU5na/HqkcTjqvg9XQKlbwSQIpIW/CXrocigNl+wuxwMlzigP7oQ==", // admin@123
+                SecurityStamp = string.Empty,
+                ConcurrencyStamp = "133257ba-cf42-4cd4-a326-5b513272e9de"
+            };
+
+            builder.Entity<IdentityUser<int>>().HasData(admin);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using VehiclePartsIMS_Backend.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +10,14 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Postgres"))
+);
+builder.Services
+    .AddIdentity<IdentityUser<int>, IdentityRole<int>>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
 
 var app = builder.Build();
 
