961bfb0545131b9e8ba87ea39a05c02821773f32
Himani Chaudhary <xhyhimani@gmail.com>
2026-04-13 22:10:23 +0545

    Create core IMS tables and entities migration

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -11,6 +11,16 @@ namespace VehiclePartsIMS_Backend.Data
         {
         }
 
+        public DbSet<Vendor> Vendors { get; set; }
+        public DbSet<Part> Parts { get; set; }
+        public DbSet<Vehicle> Vehicles { get; set; }
+        public DbSet<PurchaseInvoice> PurchaseInvoices { get; set; }
+        public DbSet<PurchaseInvoiceItem> PurchaseInvoiceItems { get; set; }
+        public DbSet<SalesInvoice> SalesInvoices { get; set; }
+        public DbSet<SalesInvoiceItem> SalesInvoiceItems { get; set; }
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<PartRequest> PartRequests { get; set; }
+
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
---
+++ Data/Entities/ServiceReview.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace VehiclePartsIMS_Backend.Data.Entities
+{
+    public class ServiceReview
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        [ForeignKey(nameof(CustomerId))]
+        public required User Customer { get; set; }
+        public int StarRating { get; set; }
+        public required string Comment { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
