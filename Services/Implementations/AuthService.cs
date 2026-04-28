c25d4e357724fa8214bf7c338f35dc533a9395ec
Safal Piya <mrsafalpiya@gmail.com>
2026-04-28 17:42:13 +0545

    Refactor auth flow, update JWT claims
    - Refactored AuthService and controller to improve HTTP response handling; LoginAsync now returns nullable DTO, controller returns proper status codes.
    - Updated JWT claim types for standards compliance and changed issuer/audience values in appsettings.json.
    - Added authentication middleware to Program.cs.

--- Services/Implementations/AuthService.cs
+++ Services/Implementations/AuthService.cs
@@ -9,35 +9,33 @@ namespace VehiclePartsIMS_Backend.Services.Implementations
 {
     public class AuthService(SignInManager<User> signInManager, UserManager<User> userManager, IJwtService jwtService) : IAuthService
     {
-        public async Task<ApiResponse<LoginResponseDto>> LoginAsync(LoginRequestDto loginRequestDto)
+        public async Task<LoginResponseDto?> LoginAsync(LoginRequestDto loginRequestDto)
         {
             var user = await userManager.FindByEmailAsync(loginRequestDto.Email);
             if (user == null)
             {
-                return ApiResponse<LoginResponseDto>.FailureResponse(["Invalid email or password."]);
+                return null;
             }
             var userRole = (await userManager.GetRolesAsync(user))[0];
             if (userRole != loginRequestDto.Role)
             {
-                return ApiResponse<LoginResponseDto>.FailureResponse(["Invalid email or password."]);
+                return null;
             }
             var signInResult = await signInManager.CheckPasswordSignInAsync(user, loginRequestDto.Password, false);
             if (!signInResult.Succeeded)
             {
-                return ApiResponse<LoginResponseDto>.FailureResponse(["Invalid email or password."]);
+                return null;
             }
 
             var jwtToken = jwtService.GenerateToken(user.Id, user.FullName, user.Email, loginRequestDto.Role);
 
-            var response = new LoginResponseDto
-            {
+            return new LoginResponseDto {
                 Token = jwtToken,
                 UserId = user.Id,
                 Name = user.FullName,
                 Email = user.Email,
                 Role = loginRequestDto.Role
             };
-            return ApiResponse<LoginResponseDto>.SuccessResponse(response);
         }
     }
 }
--- Services/Interfaces/IAuthService.cs
+++ Services/Interfaces/IAuthService.cs
@@ -1,10 +1,11 @@
-﻿using VehiclePartsIMS_Backend.Data.Dtos.Requests;
+﻿using Microsoft.AspNetCore.Mvc;
+using VehiclePartsIMS_Backend.Data.Dtos.Requests;
 using VehiclePartsIMS_Backend.Data.Dtos.Responses;
 
 namespace VehiclePartsIMS_Backend.Services.Interfaces
 {
     public interface IAuthService
     {
-        public Task<ApiResponse<LoginResponseDto>> LoginAsync(LoginRequestDto loginRequestDto);
+        public Task<LoginResponseDto?> LoginAsync(LoginRequestDto loginRequestDto);
     }
 }
